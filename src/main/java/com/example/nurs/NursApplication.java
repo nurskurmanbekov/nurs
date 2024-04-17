b2afa2f282b36d53e91f86358df278cefecc3aff
iiik <nursultan.kurmanbekov@alatoo.edu.kg>
2024-04-17 09:24:38 +0600

    Initial commit

---
+++ src/main/java/com/example/nurs/NursApplication.java
@@ -0,0 +1,13 @@
+package com.example.nurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nurs/NursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
